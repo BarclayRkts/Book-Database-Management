45c8f6b9523c9d60e4bb3c29aeb214b7c90fdfe9
DeJa <barclay.deja@gmail.com>
2021-05-26 22:00:36 -0500

    complete

--- frontend.py
+++ frontend.py
@@ -36,12 +36,9 @@ class Person:
         def display():
             results = db.displayInfo()
             bookList.delete(0,END)
-            
+
             for row in results:
                 bookList.insert(END, row)
-            
-            # list_item = bookList.get(ANCHOR)
-            # print(list_item)
 
         def clear():
             # clicked.get().delete(0, END)
@@ -54,33 +51,26 @@ class Person:
             self.borrowed_entry.delete(0, END)
             self.due_entry.delete(0, END)
         
-        def delete():
-            display()
-            list_item = bookList.curselection(sb)
-            bookList.delete(list_item)
-
-            print(list_item)
-        
         def selectedBox(event):
             global sb 
             #clickedBook = bookList.get(ANCHOR)
             clickedBook = bookList.curselection()[0]
             sb = bookList.get(clickedBook)
-
-            self.bookTitle_entry.delete(0, END)
-            self.bookTitle_entry.insert(END, sb[1])
-
+            
             self.firstName_entry.delete(0, END)
-            self.firstName_entry.insert(END, sb[2])
+            self.firstName_entry.insert(END, sb[1])
             
             self.lastName_entry.delete(0, END)
-            self.lastName_entry.insert(END, sb[3])
+            self.lastName_entry.insert(END, sb[2])
 
             self.phone_entry.delete(0, END)
-            self.phone_entry.insert(END, sb[4])
-
+            self.phone_entry.insert(END, sb[3])
+            
             self.bookID_entry.delete(0, END)
-            self.bookID_entry.insert(END, sb[5])
+            self.bookID_entry.insert(END, sb[4])
+
+            self.bookTitle_entry.delete(0, END)
+            self.bookTitle_entry.insert(END, sb[5])
 
             self.author_entry.delete(0, END)
             self.author_entry.insert(END, sb[6])
@@ -98,11 +88,9 @@ class Person:
 
         MainFrame=Frame(self.window, bg="white")
         MainFrame.grid()  #THIS IS MAIN FRAME OUR WINDOW
-        TitleFrame=Frame(MainFrame,bd=1,padx=55,pady=8,bg="black",relief=RIDGE)
-        TitleFrame.pack(side=TOP)
 
-        DataFrameRight=LabelFrame(MainFrame,font=('arial',12,'bold'),bd=1,width=450,height=300,bg="Ghost White",relief=RIDGE,text="Book Details\n")
-        DataFrameRight.pack(side=BOTTOM, padx=20,pady=20) # This is for Book Details Box
+        DetailsFrame=LabelFrame(MainFrame,font=('arial',12,'bold'),bd=1,width=450,height=300,bg="Ghost White",relief=RIDGE,text="Book Details\n")
+        DetailsFrame.pack(side=BOTTOM, padx=20,pady=20) # This is for Book Details Box
         
         ButtonFrame=Frame(MainFrame,bd=1,width=1350,height=70,padx=18,pady=10,relief=RIDGE)
         ButtonFrame.pack(side=BOTTOM)#
@@ -110,74 +98,72 @@ class Person:
         DataFrame=Frame(MainFrame,bd=9,width=1300,height=400,padx=20,pady=20,bg="white",relief=RIDGE)
         DataFrame.pack(side=BOTTOM)
 
-        DataFrameLeft=LabelFrame(DataFrame,font=('arial',12,'bold'),padx=20,pady=20, relief=RIDGE,text="Libary Membership Info:\n")
-        DataFrameLeft.pack(side=LEFT)
+        InputFrame=LabelFrame(DataFrame,font=('arial',12,'bold'),padx=20,pady=20, relief=RIDGE,text="Libary Membership Info:\n")
+        InputFrame.pack(side=LEFT)
 
 
         ##################### Inputs ################################
 
-        self.member_label = Label(DataFrameLeft, text="Membership Type: ", justify=LEFT, anchor="w")
+        self.member_label = Label(InputFrame, text="Membership Type: ", justify=LEFT, anchor="w")
         self.member_label.grid(row=0, column=0)
 
-        # self.member_entry = Entry(DataFrameLeft, width=30)
-        # self.member_entry.grid(row=0, column=1, sticky = W)
         clicked = StringVar()
         clicked.set("         ")
-        self.drop = OptionMenu(DataFrameLeft, clicked, "Administrator", "Staff", "Student")
+        self.drop = OptionMenu(InputFrame, clicked, "Administrator", "Staff", "Student")
         self.drop.config(width=25, bg="white", justify=LEFT, anchor="w")
         self.drop.grid(row=0, column=1, sticky = W)
         
-        self.firstName = Label(DataFrameLeft, text="First Name: ", justify=LEFT, anchor="w")
+        self.firstName = Label(InputFrame, text="First Name: ", justify=LEFT, anchor="w")
         self.firstName.grid(row=1, column=0, sticky = W)
 
-        self.firstName_entry = Entry(DataFrameLeft, width=30)
+        self.firstName_entry = Entry(InputFrame, width=30)
         self.firstName_entry.grid(row=1, column=1)
         
-        self.lastName = Label(DataFrameLeft, text="Last Name: ", justify=LEFT, anchor="w")
+        self.lastName = Label(InputFrame, text="Last Name: ", justify=LEFT, anchor="w")
         self.lastName.grid(row=2, column=0, sticky = W)
 
-        self.lastName_entry = Entry(DataFrameLeft, width=30)
+        self.lastName_entry = Entry(InputFrame, width=30)
         self.lastName_entry.grid(row=2, column=1)
 
-        self.phone = Label(DataFrameLeft, text="Phone Number: ", justify=LEFT, anchor="w")
+        self.phone = Label(InputFrame, text="Phone Number: ", justify=LEFT, anchor="w")
         self.phone.grid(row=3, column=0, sticky = W)
 
-        self.phone_entry = Entry(DataFrameLeft, width=30)
+        self.phone_entry = Entry(InputFrame, width=30)
         self.phone_entry.grid(row=3, column=1)
 
-        self.bookID = Label(DataFrameLeft, text="Book ID: ", justify=LEFT, anchor="w")
+        self.bookID = Label(InputFrame, text="Book ID: ", justify=LEFT, anchor="w")
         self.bookID.grid(row=4, column=0, sticky = W)
 
-        self.bookID_entry = Entry(DataFrameLeft, width=30)
+        self.bookID_entry = Entry(InputFrame, width=30)
         self.bookID_entry.grid(row=4, column=1)
 
-        self.bookTitle = Label(DataFrameLeft, text="Book Title: ", justify=LEFT, anchor="w")
+        self.bookTitle = Label(InputFrame, text="Book Title: ", justify=LEFT, anchor="w")
         self.bookTitle.grid(row=5, column=0, sticky = W)
 
-        self.bookTitle_entry = Entry(DataFrameLeft, width=30)
+        self.bookTitle_entry = Entry(InputFrame, width=30)
         self.bookTitle_entry.grid(row=5, column=1)
 
-        self.author = Label(DataFrameLeft, text="Author: ", justify=LEFT, anchor="w")
+        self.author = Label(InputFrame, text="Author: ", justify=LEFT, anchor="w")
         self.author.grid(row=6, column=0, sticky = W)
 
-        self.author_entry = Entry(DataFrameLeft, width=30)
+        self.author_entry = Entry(InputFrame, width=30)
         self.author_entry.grid(row=6, column=1)
 
-        self.borrowed = Label(DataFrameLeft, text="Date Borrowed: ", justify=LEFT, anchor="w")
+        self.borrowed = Label(InputFrame, text="Date Borrowed: ", justify=LEFT, anchor="w")
         self.borrowed.grid(row=7, column=0, sticky = W)
 
-        self.borrowed_entry = Entry(DataFrameLeft, width=30)
+        self.borrowed_entry = Entry(InputFrame, width=30)
         self.borrowed_entry.grid(row=7, column=1)
 
-        self.due = Label(DataFrameLeft, text="Date Due: ", justify=LEFT, anchor="w")
+        self.due = Label(InputFrame, text="Date Due: ", justify=LEFT, anchor="w")
         self.due.grid(row=8, column=0, sticky = W)
 
-        self.due_entry = Entry(DataFrameLeft, width=30)
+        self.due_entry = Entry(InputFrame, width=30)
         self.due_entry.grid(row=8, column=1)
 
         ##################### Book Details Box ################################
 
-        bookList=Listbox(DataFrameRight,width=100,height=12,font=('arial',12,'bold'))
+        bookList=Listbox(DetailsFrame,width=100,height=12,font=('arial',12,'bold'))
         bookList.bind('<<ListboxSelect>>', selectedBox)
         bookList.grid(row=0,column=0,padx=10)
         
@@ -192,14 +178,14 @@ class Person:
         self.clear_btn = Button(ButtonFrame, text="Clear Data", width=10, height=2, command=clear)
         self.clear_btn.grid(row=0, column=2)
         
-        self.delete_btn = Button(ButtonFrame, text="Delete", width=10, height=2, command=delete)
-        self.delete_btn.grid(row=0, column=3)
+        # self.delete_btn = Button(ButtonFrame, text="Delete", width=10, height=2, command=delete)
+        # self.delete_btn.grid(row=0, column=3)
 
-        self.update_btn = Button(ButtonFrame, text="Update", width=10, height=2)
-        self.update_btn.grid(row=0, column=4)
+        # self.update_btn = Button(ButtonFrame, text="Update", width=10, height=2)
+        # self.update_btn.grid(row=0, column=4)
         
-        self.delete_btn = Button(ButtonFrame, text="Search Data", width=10, height=2)
-        self.delete_btn.grid(row=0, column=5)
+        # self.delete_btn = Button(ButtonFrame, text="Search Data", width=10, height=2)
+        # self.delete_btn.grid(row=0, column=5)
 
         self.exit_btn = Button(ButtonFrame, text="Exit", width=10, height=2, command=exit)
         self.exit_btn.grid(row=0, column=6)
